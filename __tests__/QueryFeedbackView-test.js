0c5af747834509ae3d555f9120c50564870ebc3b
Rene van der Ark <rar011@rar011-hp-z220-sff-workstation.wpakb.kb.nl>
2015-06-15 15:13:41 +0200

    react test

---
+++ __tests__/QueryFeedbackView-test.js
@@ -0,0 +1,13 @@
+jest.dontMock('../src/components/QueryFeedbackView');
+
+describe('QueryfeedbackView', function() {
+
+	it('renders', function() {
+		var React = require('react/addons');
+		var QueryFeedbackView = require('../src/components/QueryFeedbackView');
+		var TestUtils = React.addons.TestUtils;
+ 		var component = TestUtils.renderIntoDocument( <QueryFeedbackView /> );
+ 		var div = TestUtils.findRenderedDOMComponentWithTag(component, 'div'); 
+ 		expect(div.getDOMNode().textContent).toEqual('');
+	});
+});
