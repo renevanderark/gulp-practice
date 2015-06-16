0bada320b492c92dee45409d03cda58bb8deb6a3
Rene van der Ark <r.van.der.ark@gmail.com>
2015-06-16 21:23:01 +0200

    winified2

--- watch.bat
+++ watch.bat
@@ -1,5 +1,6 @@
 md dist\js
 md dist\css
+start /B browser-sync start --files "dist/css/*.css, dist/js/*.js, index.html" --server --reload-delay 500 &
 start /B stylus --compress --out dist/css --watch style
 watchify ./src/app.js^
 	--detect-globals false^
